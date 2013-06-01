a732f53cf6364f2a7614461a0d6dfe618f3a21b7
ths <thies.schillhorn@stud.fh-luebeck.de>
2013-06-01 20:58:25 +0200

    ueberpruefen, ob displayData gesetzt ist, ansonsten gibt es eine Fehlermeldung

--- templates/content_start.php
+++ templates/content_start.php
@@ -2,9 +2,9 @@
 		
 <div class="gallery">	
 <?php
-print_r($displayData);
+// print_r($displayData);
 // es wird geprueft ob Bilder vorhanden sind, diese werden wenn vorhanden mit hilfe einer Schleife ausgegeben
-if(count($displayData['images']) > 0) foreach ($displayData['images'] as $key => $img){?>
+	if (isset($displayData['images'])) foreach ($displayData['images'] as $key => $img){?>
 <div class="startpage_image">
 	<a href="<?php echo $img->imgLink ?>">
 		<img src="<?php echo $img->thumbnail ?>">
