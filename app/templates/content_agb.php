926c8ba055fc22268769094a2fff004d72a53827
Maike2804 <maike.schr@googlemail.com>
2013-06-21 14:47:59 +0200

    agb noch angepasst
    fehler agb behoben
    ignore Datei ergänzt

--- app/templates/content_agb.php
+++ app/templates/content_agb.php
@@ -12,35 +12,59 @@
 		
 		<h3>I. Allgemeines</h3>
 			<p>
-			1. Die nachfolgenden AGB gelten f&uuml;r alle dem Fotografen erteilten Auftr&auml;ge. Sie gelten als verein-bart, wenn ihnen nicht umgehend widersprochen wird.
+			1. Die nachfolgenden AGB gelten f&uuml;r alle dem Fotografen erteilten Auftr&auml;ge. Sie gelten als vereinbart, wenn ihnen nicht umgehend widersprochen wird.
+			</p>
+			<p>
 			2. „Lichtbilder“ im Sinne dieser AGB sind alle vom Fotografen hergestellten Produkte, gleich in welcher technischen Form oder in welchem Medium sie erstellt wurden oder vorliegen. (Negative, Dia-Positive, Papierbilder, Still-Videos, elektronische Stehbilder in digitalisierter Form, Videos usw.)
 			</p>
 		<h3>II. Urheberrecht</h3>
 			<p>
-			1. Dem Fotografen steht das Urheberrecht an den Lichtbildern nach Ma&szlig;gabe des Urheberrechts-gesetzes zu.
+			1. Dem Fotografen steht das Urheberrecht an den Lichtbildern nach Ma&szlig;gabe des Urheberrechtsgesetzes zu.
+			</p>
+			<p>
 			2. Die vom Fotografen hergestellten Lichtbilder sind grunds&auml;tzlich nur f&uuml;r den eigenen Gebrauch des Auftraggebers bestimmt.
+			</p>
+			<p>
 			3. &uuml;bertr&auml;gt der Fotograf Nutzungsrechte an seinen Werken, ist – sofern nicht ausdr&uuml;cklich etwas anderes vereinbart wurde – jeweils nur das einfache Nutzungsrecht &uuml;bertragen. Eine Weitergabe von Nutzungsrechten bedarf der besonderen Vereinbarung.
+			</p>
+			<p>
 			4. Die Nutzungsrechte gehen erst &uuml;ber nach vollst&auml;ndiger Bezahlung des Honorars an den Foto-grafen.
+			</p>
+			<p>
 			5. Der Besteller eines Bildes i.S. vom § 60 UrhG hat kein Recht, das Lichtbild zu vervielf&auml;ltigen und zu verbreiten, wenn nicht die entsprechenden Nutzungsrechte &uuml;bertragen worden sind. § 60 UrhG wird ausdr&uuml;cklich abbedungen.
+			</p>
+			<p>
 			6. Bei der Verwertung der Lichtbilder kann der Fotograf, sofern nichts anderes vereinbart wurde, verlangen, als Urheber des Lichtbildes genannt zu werden. Eine Verletzung des Rechts auf Na-mensnennung berechtigt den Fotografen zum Schadensersatz.
+			</p>
+			<p>
 			7. Die Negative verbleiben beim Fotografen. Eine Herausgabe der Negative an den Auftraggeber erfolgt nur bei gesonderter Vereinbarung.
 			</p>
 		<h3>III. Verg&uuml;tung, Eigentumsvorbehalt</h3>
 			<p>
-			1. F&uuml;r die Herstellung der Lichtbilder wird ein Honorar als Stundensatz, Tage&szlig;atz oder vereinbar-te Pauschale berechnet. Nach § 19 UStG (Kleinunternehmerregelung) f&auml;llt keine Umsatzsteuer an. Nebenkosten (Reisekosten, Modellhonorare, Spesen, Requisiten, Labor- und Materialkosten, Stu-diomieten etc.) sind vom Auftraggeber zu tragen.
-			2. F&auml;llige Rechnungen sind innerhalb von 14 Tagen ohne Abzug zu zahlen. Der Auftraggeber ge-r&auml;t in Verzug, wenn er f&auml;llige Rechnungen nicht sp&auml;testens 30 (in Worten: drei&szlig;ig) Tage nach Zu-gang einer Rechnung oder gleichwertigen Zahlungsaufforderung begleicht. Dem Fotografen bleibt vorbehalten, den Verzug durch Erteilung einer nach F&auml;lligkeit zugehenden Mahnung zu einem fr&uuml;heren Zeitpunkt herbeizuf&uuml;hren.
+			1. F&uuml;r die Herstellung der Lichtbilder wird ein Honorar als Stundensatz, Tagessatz oder vereinbarte Pauschale berechnet. Nach § 19 UStG (Kleinunternehmerregelung) f&auml;llt keine Umsatzsteuer an. Nebenkosten (Reisekosten, Modellhonorare, Spesen, Requisiten, Labor- und Materialkosten, Studiomieten etc.) sind vom Auftraggeber zu tragen.
+			</p>
+			<p>
+			2. F&auml;llige Rechnungen sind innerhalb von 14 Tagen ohne Abzug zu zahlen. Der Auftraggeber ger&auml;t in Verzug, wenn er f&auml;llige Rechnungen nicht sp&auml;testens 30 (in Worten: drei&szlig;ig) Tage nach Zugang einer Rechnung oder gleichwertigen Zahlungsaufforderung begleicht. Dem Fotografen bleibt vorbehalten, den Verzug durch Erteilung einer nach F&auml;lligkeit zugehenden Mahnung zu einem fr&uuml;heren Zeitpunkt herbeizuf&uuml;hren.
+			</p>
+			<p>
 			3. Bis zur vollst&auml;ndigen Bezahlung des Kaufpreises bleiben die gelieferten Lichtbilder Eigentum des Fotografen.
-			4. Hat der Auftraggeber dem Fotografen keine ausdr&uuml;cklichen Weisungen hinsichtlich der Gestal-tung der Lichtbilder gegeben, so sind Reklamationen bez&uuml;glich der Bildauffa&szlig;ung sowie der k&uuml;nstlerisch-technischen Gestaltung ausgeschlo&szlig;en. W&uuml;nscht der Auftraggeber w&auml;hrend oder nach der Aufnahmeproduktion &auml;nderungen, so hat er die Mehrkosten zu tragen. Der Fotograf be-h&auml;lt den Verg&uuml;tungs-Anspruch f&uuml;r bereits begonnene Arbeiten.
+			</p>
+			<p>
+			4. Hat der Auftraggeber dem Fotografen keine ausdr&uuml;cklichen Weisungen hinsichtlich der Gestaltung der Lichtbilder gegeben, so sind Reklamationen bez&uuml;glich der Bildauffa&szlig;ung sowie der k&uuml;nstlerisch-technischen Gestaltung ausgeschlo&szlig;en. W&uuml;nscht der Auftraggeber w&auml;hrend oder nach der Aufnahmeproduktion &auml;nderungen, so hat er die Mehrkosten zu tragen. Der Fotograf beh&auml;lt den Verg&uuml;tungs-Anspruch f&uuml;r bereits begonnene Arbeiten.
 			</p>
 		<h3>IV. Haftung</h3>
 			<p>
-			F&uuml;r die Verletzung von Pflichten, die nicht in unmittelbarem Zusammenhang mit wesentlichen Ver-tragspflichten stehen, haftet der Fotograf f&uuml;r sich und seine Erf&uuml;llungsgehilfen nur bei Vorsatz und grober Fahrl&auml;&szlig;igkeit.
-			</p><
+			F&uuml;r die Verletzung von Pflichten, die nicht in unmittelbarem Zusammenhang mit wesentlichen Vertragspflichten stehen, haftet der Fotograf f&uuml;r sich und seine Erf&uuml;llungsgehilfen nur bei Vorsatz und grober Fahrl&auml;&szlig;igkeit.
+			</p>
 		<h3>V. Leistung&szlig;t&ouml;rung, Ausfallhonorar</h3>
 			<p>
 			1. Wird die f&uuml;r die Durchf&uuml;hrung des Auftrages vorgesehene Zeit aus Gr&uuml;nden, die der Fotograf nicht zu vertreten hat, wesentlich &uuml;berschritten, so erh&ouml;ht sich das Honorar des Fotografen, sofern ein Pauschalpreis vereinbart war, entsprechend. Ist ein Zeithonorar vereinbart, erh&auml;lt der Fotograf auch f&uuml;r die Wartezeit den vereinbarten Stunden- oder Tage&szlig;atz, sofern nicht der Auftraggeber nachweist, da&szlig; dem Fotografen kein Schaden entstanden ist. Bei Vorsatz oder Fahrl&auml;&szlig;igkeit des Auftraggebers kann der Fotograf auch Schadensersatzanspr&uuml;che geltend machen.
+			</p>
+			<p>
 			2. Liefertermine f&uuml;r Lichtbilder sind nur dann verbindlich, wenn sie ausdr&uuml;cklich vom Fotografen best&auml;tigt worden sind. Der Fotograf haftet f&uuml;r Frist&uuml;berschreitung nur bei Vorsatz und grober Fahr-l&auml;&szlig;igkeit.
-			3. Tritt der Kunde mit Einverst&auml;ndnis der Fotografen vor dem vereinbarten Fototermin vom Vertrag zur&uuml;ck, so sind 75% der vereinbarten Summe als Ausfallhonorar zu zahlen. Gesetzliche R&uuml;cktritts-rechte bleiben von dieser Regelung unber&uuml;hrt.
+			</p>
+			<p>
+			3. Tritt der Kunde mit Einverst&auml;ndnis der Fotografen vor dem vereinbarten Fototermin vom Vertrag zur&uuml;ck, so sind 75% der vereinbarten Summe als Ausfallhonorar zu zahlen. Gesetzliche R&uuml;cktrittsrechte bleiben von dieser Regelung unber&uuml;hrt.
 			</p>
 		<h3>VI. Datenschutz</h3>
 		
@@ -57,10 +81,15 @@
 			Die Bearbeitung von Lichtbildern des Fotografen und ihre Vervielf&auml;ltigung und Verbreitung, analog oder digital, bedarf der vorherigen Zustimmung des Fotografen.
 			<p>
 		<h3>IX. Nutzung und Verbreitung</h3>
+			<p>1. Die Verbreitung von Lichtbildern des Fotografen im Internet und in Intranets, in Online-Datenbanken, in elektronischen Archiven, die nicht nur f&uuml;r den internen Gebrauch des Auftragge-bers bestimmt sind, auf Diskette, CD-ROM oder &auml;hnlichen Datentr&auml;gern ist nur aufgrund einer besonderen Vereinbarung zwischen dem Fotografen und dem Auftraggeber gestattet.
+			</p>
 			<p>
-			1. Die Verbreitung von Lichtbildern des Fotografen im Internet und in Intranets, in Online-Datenbanken, in elektronischen Archiven, die nicht nur f&uuml;r den internen Gebrauch des Auftragge-bers bestimmt sind, auf Diskette, CD-ROM oder &auml;hnlichen Datentr&auml;gern ist nur aufgrund einer besonderen Vereinbarung zwischen dem Fotografen und dem Auftraggeber gestattet.
 			2. Bei privaten Fotodienstleistungen ist die Weitergabe der Lichtbilder an Freunde und Verwandte zu deren privatem Gebrauch jederzeit gestattet.
+			</p>
+			<p>
 			3. Die Weitergabe digitalisierter Lichtbilder im Internet und in Intranets und auf Datentr&auml;gern und Ger&auml;ten, die zur &ouml;ffentlichen Wiedergabe auf Bildschirmen oder zur Herstellung von Soft- und Hardcopies geeignet sind, bedarf der vorherigen schriftlichen Zustimmung des Fotografen.
+			</p>
+			</p>
 			4. Die Vervielf&auml;ltigung und Verbreitung von Bearbeitungen, die der Fotograf auf elektronischem Wege hergestellt hat, bed&uuml;rfen der vorherigen schriftlichen Zustimmung des Fotografen.
 			</p>
 		<h3>X. Schlu&szlig;bestimmungen</h3>
